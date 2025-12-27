ea93833e27a2185dcd944c9e2a834dcd77aa036d
melarkkkk <melark@192-168-100-126.lan>
2025-12-27 10:09:23 +0100

    add task solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,31 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should return true for an empty line`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`should return true if there's no repeating letters`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+
+    expect(isIsogram('oops'))
+      .toBe(false);
+
+    expect(isIsogram('adam'))
+      .toBe(false);
+  });
+
+  it(`should be case insensitive`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+
+    expect(isIsogram('oOps'))
+      .toBe(false);
+
+    expect(isIsogram('bOo'))
+      .toBe(false);
+  });
 });
